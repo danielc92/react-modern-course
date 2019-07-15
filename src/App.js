7091a554b70a7437c25ee75622b93a69c28d00b3
Daniel Corcoran <danielc.92@outlook.com>
2019-07-15 14:38:30 +1000

    Added api call for geolocation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ export default class extends Component {
 
     constructor (props) {
         super(props);
-        state = {}
+        this.state = {
+            coordinates: {}
+        }
+
+        window.navigator.geolocation.getCurrentPosition(
+            (results) => {
+                this.setState({coordinates: results.coords})
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
     }
 
     render() {
+        // Dont put any heavy functions in here
+        // render() gets called frequently...   
+        console.log('render this')
         return (
             <div>
-
+                <h3>Location</h3>
+                <p>{this.state.coordinates.longitude}</p>
             </div>
         )
     }
