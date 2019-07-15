2a1c95544e956619e6b84ad172eb460454bdb7a7
Daniel Corcoran <danielc.92@outlook.com>
2019-07-15 11:29:12 +1000

    Added semantic cdn

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// crete component
-class App extends React.Component {
-    render () {
-        return (
-            <div>
-                <h3>App Name</h3>
-            </div>
-        )
-    }
+// create component
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    )
 }
 
 
