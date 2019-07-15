7aa6cc2225656c1f0027411f4c5cd00475268491
Daniel Corcoran <danielc.92@outlook.com>
2019-07-15 11:40:53 +1000

    Added faker module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
 // import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 
 // create component
 const App = () => {
+
+    const image = faker.image.avatar()
+    console.log(image);
+
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img src="" alt="avatar"/>
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+                <section className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post
+                    </div>
+                </section>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+                <section className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post
+                    </div>
+                </section>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
                 </a>
                 <section className="content">
                     <a href="/" className="author">
