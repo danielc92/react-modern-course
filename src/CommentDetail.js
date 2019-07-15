4f0893ebf56ba7cf7378001ee1824254961b2190
Daniel Corcoran <danielc.92@outlook.com>
2019-07-15 12:54:53 +1000

    Added props for comment

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
 const CommentDetail = (props) => {
-    
-    return (
-        <React.Fragment>
-        <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={props.avatar} alt="avatar"/>
-                </a>
-                <section className="content">
-                    <a href="/" className="author">
-                        {props.author}
-                    </a>
-                    <div className="metadata">
-                        <span className="date">{props.postedAt}</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </section>
-            </div>
-            </React.Fragment>
-    )
-}
 
+  return (
+    <React.Fragment>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={props.avatar} alt="avatar"/>
+        </a>
+        <section className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{props.postedAt}</span>
+          </div>
+          <div className="text">
+            {props.commentText}
+          </div>
+        </section>
+      </div>
+    </React.Fragment>
+  )
+}
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
-const { image } = faker;
+const {image} = faker;
 
 // Create Component
 const App = () => {
 
-    return (
-        <div className="ui container comments">
-            <CommentDetail avatar={image.avatar()} author={'Duke'} postedAt={"Today at 4.45PM"}/>
-            <CommentDetail avatar={image.avatar()} author={'Lucy'} postedAt={"Today at 5.45PM"}/>
-            <CommentDetail avatar={image.avatar()} author={'Ruby'} postedAt={"Today at 6.15PM"}/>
-            <CommentDetail avatar={image.avatar()} author={'Jay'} postedAt={"Today at 4.25PM"}/>
-        </div>
-    )
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        commentText={"Nice blog post!"}
+        avatar={image.avatar()}
+        author={'Duke'}
+        postedAt={"Today at 4.45PM"}/>
+      <CommentDetail
+        commentText={"What is this?"}
+        avatar={image.avatar()}
+        author={'Lucy'}
+        postedAt={"Today at 5.45PM"}/>
+      <CommentDetail
+        commentText={"I like the writing!"}
+        avatar={image.avatar()}
+        author={'Ruby'}
+        postedAt={"Today at 6.15PM"}/>
+      <CommentDetail
+        commentText={"Cool blog post!"}
+        avatar={image.avatar()}
+        author={'Jay'}
+        postedAt={"Today at 4.25PM"}/>
+    </div>
+  )
 }
 
-
 // take component and render
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+  <App/>, document.getElementById('root'))
