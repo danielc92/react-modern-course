46f50df880280f7b67246b9120ab7feaa79afc37
Daniel Corcoran <danielc.92@outlook.com>
2019-07-16 09:59:24 +1000

    removed state constructor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react'
 
 export default class extends Component {
 
-    constructor (props) {
-        super(props);
-        this.state = {
+    state = {
             coordinates: {},
             error: null
-        }
     }
 
     componentDidMount() {
