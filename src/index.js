eeab39643931bdc0af81d57bc35b3c542709455d
Daniel Corcoran <danielc.92@outlook.com>
2019-07-15 12:33:05 +1000

    Split out commentdetail

--- src/index.js
+++ src/index.js
@@ -1,67 +1,15 @@
 // import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 
 // create component
 const App = () => {
 
-    const image = faker.image.avatar()
-    console.log(image);
-
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar"/>
-                </a>
-                <section className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </section>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar"/>
-                </a>
-                <section className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </section>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar"/>
-                </a>
-                <section className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post
-                    </div>
-                </section>
-            </div>
+            <CommentDetail/>
         </div>
     )
 }
