b4526c84805822d8ad1d87536cff5583c3a28273
Daniel Corcoran <danielc.92@outlook.com>
2019-07-16 11:06:17 +1000

    Added styling to SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="ui container" style={{marginTop: '2rem'}}>
+                <SearchBar/>
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+    
+    state = {
+        userInput: null
+    }
+    
     render() {
         return (
-            <div>
-                
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                    <label>Image Search</label>
+                    <input type="text"></input>
+                    </div>
+                    
+                </form>
             </div>
         )
     }
