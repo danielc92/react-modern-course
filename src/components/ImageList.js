1040c7b1a686aab0f6f378d177a579964ea13200
Daniel Corcoran <danielc.92@outlook.com>
2019-07-16 14:39:24 +1000

    Refactored image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,18 +3,25 @@ import React, { Component } from 'react'
 export default class ImageList extends Component {
     render() {
         return (
+            <React.Fragment>
             <section className="ui three column grid">
                 {
                     this.props.images.map(image => {
                         return(
                             <div className="column">
                                 <div className="ui segment">
-                                <img className="ui fluid image" key={image.id} src={image.urls.regular}></img>
+                                    <img 
+                                    className="ui image" 
+                                    key={image.id} 
+                                    src={image.urls.regular}
+                                    >
+                                    </img>
                                 </div>
                             </div>
                     )})
                 }
             </section>
+            </React.Fragment>
         )
     }
 }
