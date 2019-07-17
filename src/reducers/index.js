598be0a10a6addb3cae37a6fba574375838bc910
Daniel Corcoran <danielc.92@outlook.com>
2019-07-17 15:32:14 +1000

    Added songsReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const songsReducer = () => {
+    return [
+        { title: 'Dolore anim ullamco do consectetur esse reprehenderit velit cupidatat consequat occaecat magna proident.', duration: '2:34'},
+        { title: 'Deserunt non qui deserunt ea.', duration: '2:59'},
+        { title: 'Occaecat cupidatat amet proident elit aliquip dolor elit ipsum id sunt est non ex deserunt.', duration: '3.44'},
+        { title: 'Proident id fugiat dolor aliquip irure consectetur ad occaecat proident tempor labore officia.', duration: '1:44'},
+        { title: 'Ut aliquip ipsum deserunt aliqua Lorem excepteur.', duration: '3:34'}
+    ]
+}
