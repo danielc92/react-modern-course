69e449aa86acb43ed208f423b2028956f3725b4d
Daniel Corcoran <danielc.92@outlook.com>
2019-07-18 14:01:32 +1000

    Added own props to mapstatetoprosp in userheader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -6,17 +6,24 @@ import { fetchUser } from '../actions';
 class UserHeader extends Component {
 
     componentDidMount() {
-        this.props.fetchUser(2)
+        this.props.fetchUser(this.props.userId)
     }
 
     render() {
+        console.log(this.props)
         return (
             <div>
-                <h3>User Header {this.props.userId}</h3>
+                <h3>User Header { this.props.userId }</h3>
+                <h3>{ this.props.user ? this.props.user.name : 'user is undefined'  }</h3>
             </div>
         )
     }
 }
 
-
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+    return {
+        user: state.users.find(u => u.id === ownProps.userId)
+    }
+}
+//
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
