748adc78cb298fbfcbab25b6f8116b46971d0892
Daniel Corcoran <danielc.92@outlook.com>
2019-07-18 12:15:01 +1000

    Rendered posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,15 +7,30 @@ import { thisTypeAnnotation } from '@babel/types';
 class PostList extends Component {
     
     componentDidMount () {
-        console.log(this.props.fetchPosts())    
+        this.props.fetchPosts()
     }
     
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user"></i>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-        return (
-            <div>
-                <h1>post list</h1>
-            </div>
-        )
+        console.log('oagihoeug', this.props.posts)
+        return <section className="ui relaxed divided list">
+            {this.renderList()}
+        </section>
     }
 }
 
