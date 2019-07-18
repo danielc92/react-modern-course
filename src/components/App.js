40877d6b0c0b6801bd54e5f7578cb79e988ff98d
Daniel Corcoran <danielc.92@outlook.com>
2019-07-18 10:23:15 +1000

    Added index.js for reducers and actions

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                
+                <h1>App</h1>
             </div>
         )
     }
